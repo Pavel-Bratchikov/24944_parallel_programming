1705fed53031758e733c5fdcae09235505385591
pavel <p.bratchikov@g.nsu.ru>
2026-02-10 23:56:43 +0700

    just something chek

---
+++ 1/build/CMakeFiles/sinsum.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sinsum.dir/sinsum.cpp.o"
+  "CMakeFiles/sinsum.dir/sinsum.cpp.o.d"
+  "sinsum"
+  "sinsum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sinsum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1/build/CMakeFiles/sinsum.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sinsum.
+# This may be replaced when dependencies are built.
